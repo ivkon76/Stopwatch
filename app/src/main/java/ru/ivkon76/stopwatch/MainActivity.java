a1a76341494fb0bc1292fb6908b00334fb80eb7b
LowVolt <iv.kon76@gmail.com>
2021-04-08 13:35:49 +0300

    Глава 4. Страртовый коммит

---
+++ app/src/main/java/ru/ivkon76/stopwatch/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.ivkon76.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
